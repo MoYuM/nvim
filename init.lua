f274285d6219fc54890ae5bb4ff54118d21b731e
MoYuM <hanbaochang12138@gmail.com>
2023-01-07 18:50:17 +0800

    set fzf source

--- init.lua
+++ init.lua
@@ -6,7 +6,13 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug 'easymotion/vim-easymotion'
 Plug 'bling/vim-airline'
-Plug 'junegunn/fzf'
-Plug 'vim-ctrlspace/vim-ctrlspace'
+Plug('junegunn/fzf', {
+	['do'] = function()
+		vim.call('fzf#install')
+		vim.call('fzf#run', { source = 'git ls-files' })
+	end
+})
+Plug 'junegunn/fzf.vim'Plug 'vim-ctrlspace/vim-ctrlspace'
 
 vim.call('plug#end')
+
