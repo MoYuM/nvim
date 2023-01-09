8a87a25705a3a4f1830db153f9697bda30d7a660
MoYuM <hanbaochang12138@gmail.com>
2023-01-10 00:02:30 +0800

    keymap

--- lua/keybingdings.lua
+++ lua/keybingdings.lua
@@ -1,21 +1,26 @@
 -- leader
 vim.g.mapleader = " "
 
-vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set("i", "jj", "<Esc>")
 
+
+-- vscode
+vim.keymap.set({ 'n', 'i' }, '<D-x>', 'dd')
+
+-- paste and yank
+vim.keymap.set({ "n" }, "<D-v>", '"+p<CR>')
+vim.keymap.set('!', '<D-v>', '<C-R>+')
+vim.keymap.set('t', '<D-v>', '<C-R>+')
+vim.keymap.set('v', '<D-v>', '<C-R>+')
 
 -- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<c-p>', builtin.git_files, {})
-
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<D-p>", builtin.git_files, {})
 
 -- hop
-local hop = require('hop')
-local directions = require('hop.hint').HintDirection
-vim.keymap.set('n', 'F', hop.hint_char1, {remap=true})
-vim.keymap.set('n', 'f', function()
-  hop.hint_char1({current_line_only=true})
-end, {remap=true})
+local hop = require("hop")
+local directions = require("hop.hint").HintDirection
+vim.keymap.set("n", "<leader>s", hop.hint_char1, { remap = true })
 
 -- nvim-tree
-vim.keymap.set('n', '1', ':NvimTreeToggle<Enter>')
+vim.keymap.set("n", "1", ":NvimTreeToggle<Enter>")
