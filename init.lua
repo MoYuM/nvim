e4b1c723d6d3f54f69ad796626dac9123f167774
MoYuM <hanbaochang12138@gmail.com>
2023-01-09 22:17:45 +0800

    add nvim-tree

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 vim.g.coq_settings = {
-auto_start = 'shut-up'
+	auto_start = 'shut-up'
 }
 
 require('keybingdings')
@@ -14,3 +14,36 @@ vim.cmd[[colorscheme tokyonight]]
 -- let g:neovide_floating_blur_amount_x = 2.0
 -- let g:neovide_floating_blur_amount_y = 2.0
 -- ]]
+--
+
+-- vim
+vim.cmd[[
+set number
+set autoindent expandtab tabstop=2 shiftwidth=2
+]]
+
+-- nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,97 +4,105 @@ return require('packer').startup({function(use)
   use "nvim-lua/plenary.nvim"
   use 'folke/tokyonight.nvim'
 
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
-use {
-  'phaazon/hop.nvim',
-  branch = 'v2', -- optional but strongly recommended
-  config = function()
-    -- you can configure Hop the way you like here; see :h hop-config
-    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-  end
-}
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
 
-use {
-  'rmagatti/auto-session',
-  config = function()
-    require("auto-session").setup {
-      log_level = "error",
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-    }
-  end
-}
+  use {
+    'rmagatti/auto-session',
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+      }
+    end
+  }
 
-use 'f-person/git-blame.nvim'
+  use 'f-person/git-blame.nvim'
 
 
-use {
-  'ms-jpq/coq_nvim',
-  branch = 'coq',
-}
+  use {
+    'ms-jpq/coq_nvim',
+    branch = 'coq',
+  }
 
-use {
-  'ms-jpq/coq.artifacts',
-  branch = 'artifacts'
-}
+  use {
+    'ms-jpq/coq.artifacts',
+    branch = 'artifacts'
+  }
 
-use({
-	"Pocco81/auto-save.nvim",
-	config = function()
-		 require("auto-save").setup{
-    enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-    execution_message = {
-		message = function() -- message to print on save
-			return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-		end,
-		dim = 0.18, -- dim the color of `message`
-		cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-	},
-    trigger_events = {"InsertLeave", "TextChanged"}, -- vim events that trigger auto-save. See :h events
-	-- function that determines whether to save the current buffer or not
-	-- return true: if buffer is ok to be saved
-	-- return false: if it's not ok to be saved
-	condition = function(buf)
-		local fn = vim.fn
-		local utils = require("auto-save.utils.data")
+  use({
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup{
+        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+        execution_message = {
+          message = function() -- message to print on save
+            return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+          end,
+          dim = 0.18, -- dim the color of `message`
+          cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+        },
+        trigger_events = {"InsertLeave", "TextChanged"}, -- vim events that trigger auto-save. See :h events
+        -- function that determines whether to save the current buffer or not
+        -- return true: if buffer is ok to be saved
+        -- return false: if it's not ok to be saved
+        condition = function(buf)
+          local fn = vim.fn
+          local utils = require("auto-save.utils.data")
 
-		if
-			fn.getbufvar(buf, "&modifiable") == 1 and
-			utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-			return true -- met condition(s), can save
-		end
-		return false -- can't save
-	end,
-    write_all_buffers = false, -- write all buffers when the current one meets `condition`
-    debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
-	callbacks = { -- functions to be executed at different intervals
-		enabling = nil, -- ran when enabling auto-save
-		disabling = nil, -- ran when disabling auto-save
-		before_asserting_save = nil, -- ran before checking `condition`
-		before_saving = nil, -- ran before doing the actual save
-		after_saving = nil -- ran after doing the actual save
-	}
-}	end,
-})
+          if
+            fn.getbufvar(buf, "&modifiable") == 1 and
+            utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+            return true -- met condition(s), can save
+          end
+          return false -- can't save
+        end,
+        write_all_buffers = false, -- write all buffers when the current one meets `condition`
+        debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+        callbacks = { -- functions to be executed at different intervals
+        enabling = nil, -- ran when enabling auto-save
+        disabling = nil, -- ran when disabling auto-save
+        before_asserting_save = nil, -- ran before checking `condition`
+        before_saving = nil, -- ran before doing the actual save
+        after_saving = nil -- ran after doing the actual save
+      }
+    }	end,
+  })
 
-use {
+  use {
     'numToStr/Comment.nvim',
     config = function()
-        require('Comment').setup()
+      require('Comment').setup()
     end
-}
+  }
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+
+use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
 
 end,
 config = {
