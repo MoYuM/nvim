8bc99057ebe8ed2646e13145c026584690ebe55b
MoYuM <hanbaochang12138@gmail.com>
2023-01-09 23:44:17 +0800

    format

--- init.lua
+++ init.lua
@@ -54,3 +54,4 @@ require("nvim-tree").setup({
 vim.g.neoformat_basic_format_align = 1
 vim.g.neoformat_basic_format_retab = 1
 vim.g.neoformat_basic_format_trim = 1
+vim.g.neoformat_run_all_formatters = 1
