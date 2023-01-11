c4d6fc67f25b89d5359924e89f1528a68abbd33b
MoYuM <hanbaochang12138@gmail.com>
2023-01-11 22:59:55 +0800

    add treesitter

--- init.lua
+++ init.lua
@@ -95,3 +95,26 @@ vim.diagnostic.config({
 
 -- lspsaga
 require("lspsaga").init_lsp_saga()
+
+-- treesitter
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"lua",
+		"javascript",
+		"typescript",
+		"css",
+		"html",
+	},
+	sync_install = true,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -154,6 +154,15 @@ return require("packer").startup({
 				require("gitsigns").setup()
 			end,
 		})
+
+    -- treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
 	end,
 
 	config = {
