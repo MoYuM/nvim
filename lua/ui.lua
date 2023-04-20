99f6a4759c740df47854759affe2b898e3436270
MoYuM <hanbaochang12138@gmail.com>
2023-04-20 14:44:27 +0800

    feat: 去掉尾部的箭头

--- lua/ui.lua
+++ lua/ui.lua
@@ -8,7 +8,7 @@ vim.o.guifont = "CaskaydiaCove Nerd Font Mono"
 
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+-- vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
