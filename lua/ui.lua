6e09799f68f93a0d39c1a1e5483cd980499971a7
MoYuM <hanbaochang12138@gmail.com>
2023-04-22 10:08:09 +0800

    feat: set ui in neovide

--- lua/ui.lua
+++ lua/ui.lua
@@ -4,12 +4,23 @@ vim.cmd[[
   set signcolumn=yes
 ]]
 
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+
+if (vim.g.neovide) then
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.g.neovide_floating_blur_amount_x = 4.0
+  vim.g.neovide_floating_blur_amount_y = 4.0
+  vim.g.neovide_remember_window_size = true
+end
+
 vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h16"
 vim.opt.linespace = 2
 
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
