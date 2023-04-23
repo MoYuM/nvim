0a5a2e5b56ce83e796da8636e89333e1f91491fc
MoYuM <hanbaochang12138@gmail.com>
2023-04-23 23:46:14 +0800

    feat: 去掉下划线的部分

--- lua/ui.lua
+++ lua/ui.lua
@@ -14,7 +14,6 @@ vim.opt.listchars:append "space:⋅"
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
-    show_current_context_start = true,
 }
 -- lualine
 -- https://github.com/nvim-lualine/lualine.nvim
