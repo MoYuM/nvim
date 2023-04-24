82fe58c5fbd34bf4ec9eccdd00a0af9cf73eecc3
wb-hbc831417 <wb-hbc831417@alibaba-inc.com>
2023-04-24 19:09:26 +0800

    0.2

--- init.lua
+++ init.lua
@@ -1,20 +1,20 @@
 require("plugins")
-require("ui")
-require('postfix')
-require('lsp')
-require('tree')
-require('cmpsetup')
-require("lspsaga").setup({})
-require("treesitter")
-require("keybingdings")
+-- require("ui")
+-- require('postfix')
+-- require('lsp')
+-- require('tree')
+-- require('cmpsetup')
+-- require("lspsaga").setup({})
+-- require("treesitter")
+-- require("keybingdings")
 
--- vim
-vim.cmd([[
-set nowrap
-set autoindent expandtab tabstop=2 shiftwidth=2
-set completeopt=menu,menuone,noselect
-augroup packer_user_config
-autocmd!
-autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-augroup end
-]])
+-- -- vim
+-- vim.cmd([[
+-- set nowrap
+-- set autoindent expandtab tabstop=2 shiftwidth=2
+-- set completeopt=menu,menuone,noselect
+-- augroup packer_user_config
+-- autocmd!
+-- autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+-- augroup end
+-- ]])
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,169 +13,169 @@ local packer_bootstrap = ensure_packer()
 
 return require("packer").startup({
   function(use)
-    use("wbthomason/packer.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("folke/tokyonight.nvim")
-    use("f-person/git-blame.nvim")
-    use("sbdchd/neoformat")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("hrsh7th/cmp-cmdline")
-    use("hrsh7th/nvim-cmp")
-    use("L3MON4D3/LuaSnip")
-    use("saadparwaiz1/cmp_luasnip")
-    use("windwp/nvim-ts-autotag")
-    use("github/copilot.vim")
-    use("tpope/vim-fugitive")
-    use('nvim-tree/nvim-web-devicons')
-    use('JoosepAlviste/nvim-ts-context-commentstring')
-    use('kdheepak/lazygit.nvim')
-    use('lukas-reineke/indent-blankline.nvim')
-    use('fedepujol/move.nvim')
-    use({
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-    })
-    use {
-      "cshuaimin/ssr.nvim",
-      module = "ssr",
-    }
+    -- use("wbthomason/packer.nvim")
+    -- use("nvim-lua/plenary.nvim")
+    -- use("folke/tokyonight.nvim")
+    -- use("f-person/git-blame.nvim")
+    -- use("sbdchd/neoformat")
+    -- use("hrsh7th/cmp-nvim-lsp")
+    -- use("hrsh7th/cmp-buffer")
+    -- use("hrsh7th/cmp-path")
+    -- use("hrsh7th/cmp-cmdline")
+    -- use("hrsh7th/nvim-cmp")
+    -- use("L3MON4D3/LuaSnip")
+    -- use("saadparwaiz1/cmp_luasnip")
+    -- use("windwp/nvim-ts-autotag")
+    -- use("github/copilot.vim")
+    -- use("tpope/vim-fugitive")
+    -- use('nvim-tree/nvim-web-devicons')
+    -- use('JoosepAlviste/nvim-ts-context-commentstring')
+    -- use('kdheepak/lazygit.nvim')
+    -- use('lukas-reineke/indent-blankline.nvim')
+    -- use('fedepujol/move.nvim')
+    -- use({
+    --   "williamboman/mason.nvim",
+    --   "williamboman/mason-lspconfig.nvim",
+    --   "neovim/nvim-lspconfig",
+    -- })
+    -- use {
+    --   "cshuaimin/ssr.nvim",
+    --   module = "ssr",
+    -- }
 
-    use({
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.0",
-      requires = { { "nvim-lua/plenary.nvim" } },
-    })
+    -- use({
+    --   "nvim-telescope/telescope.nvim",
+    --   tag = "0.1.0",
+    --   requires = { { "nvim-lua/plenary.nvim" } },
+    -- })
 
-    use({
-      "phaazon/hop.nvim",
-      branch = "v2", -- optional but strongly recommended
-      config = function()
-        require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-      end,
-    })
+    -- use({
+    --   "phaazon/hop.nvim",
+    --   branch = "v2", -- optional but strongly recommended
+    --   config = function()
+    --     require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+    --   end,
+    -- })
 
-    use({
-      "rmagatti/auto-session",
-      config = function()
-        require("auto-session").setup({
-          log_level = "error",
-          auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-        })
-      end,
-    })
-    -- auto save
-    use({
-      "Pocco81/auto-save.nvim",
-      config = function()
-        require("auto-save").setup({
-          enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-          execution_message = {
-            message = function() -- message to print on save
-              return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-            end,
-            dim = 0.18, -- dim the color of `message`
-            cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-          },
-          trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
-          condition = function(buf)
-            local fn = vim.fn
-            local utils = require("auto-save.utils.data")
+    -- use({
+    --   "rmagatti/auto-session",
+    --   config = function()
+    --     require("auto-session").setup({
+    --       log_level = "error",
+    --       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    --     })
+    --   end,
+    -- })
+    -- -- auto save
+    -- use({
+    --   "Pocco81/auto-save.nvim",
+    --   config = function()
+    --     require("auto-save").setup({
+    --       enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+    --       execution_message = {
+    --         message = function() -- message to print on save
+    --           return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+    --         end,
+    --         dim = 0.18, -- dim the color of `message`
+    --         cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+    --       },
+    --       trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
+    --       condition = function(buf)
+    --         local fn = vim.fn
+    --         local utils = require("auto-save.utils.data")
 
-            if
-              fn.getbufvar(buf, "&modifiable") == 1
-              and utils.not_in(fn.getbufvar(buf, "&filetype"), {})
-              then
-                return true -- met condition(s), can save
-              end
-              return false -- can't save
-            end,
+    --         if
+    --           fn.getbufvar(buf, "&modifiable") == 1
+    --           and utils.not_in(fn.getbufvar(buf, "&filetype"), {})
+    --           then
+    --             return true -- met condition(s), can save
+    --           end
+    --           return false -- can't save
+    --         end,
             
-            write_all_buffers = false, -- write all buffers when the current one meets `condition`
-            debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
-            callbacks = { -- functions to be executed at different intervals
-            enabling = nil, -- ran when enabling auto-save
-            disabling = nil, -- ran when disabling auto-save
-            before_asserting_save = nil, -- ran before checking `condition`
-            before_saving = function()
-              vim.call("Neoformat")
-            end, -- ran before doing the actual save
-            after_saving = nil, -- ran after doing the actual save
-          },
-        })
-      end,
-    })
+    --         write_all_buffers = false, -- write all buffers when the current one meets `condition`
+    --         debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+    --         callbacks = { -- functions to be executed at different intervals
+    --         enabling = nil, -- ran when enabling auto-save
+    --         disabling = nil, -- ran when disabling auto-save
+    --         before_asserting_save = nil, -- ran before checking `condition`
+    --         before_saving = function()
+    --           vim.call("Neoformat")
+    --         end, -- ran before doing the actual save
+    --         after_saving = nil, -- ran after doing the actual save
+    --       },
+    --     })
+    --   end,
+    -- })
 
-    -- comment
-    use({
-      "numToStr/Comment.nvim",
-      config = function()
-        require("Comment").setup({
-          pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-        })
-      end,
-    })
+    -- -- comment
+    -- use({
+    --   "numToStr/Comment.nvim",
+    --   config = function()
+    --     require("Comment").setup({
+    --       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    --     })
+    --   end,
+    -- })
 
-    -- explore
-    use({
-      "nvim-tree/nvim-tree.lua",
-      requires = {
-        "nvim-tree/nvim-web-devicons", -- optional, for file icons
-      },
-      tag = "nightly", -- optional, updated every week. (see issue #1193)
-    })
+    -- -- explore
+    -- use({
+    --   "nvim-tree/nvim-tree.lua",
+    --   requires = {
+    --     "nvim-tree/nvim-web-devicons", -- optional, for file icons
+    --   },
+    --   tag = "nightly", -- optional, updated every week. (see issue #1193)
+    -- })
 
-    -- diagnostics
-    use({
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-    })
+    -- -- diagnostics
+    -- use({
+    --   "folke/trouble.nvim",
+    --   requires = "kyazdani42/nvim-web-devicons",
+    -- })
 
-    -- auto pairs
-    use({
-      "windwp/nvim-autopairs",
-      config = function()
-        require("nvim-autopairs").setup({})
-      end,
-    })
+    -- -- auto pairs
+    -- use({
+    --   "windwp/nvim-autopairs",
+    --   config = function()
+    --     require("nvim-autopairs").setup({})
+    --   end,
+    -- })
 
-    -- lspsaga
-    use({
-      "glepnir/lspsaga.nvim",
-      branch = "main",
-    })
+    -- -- lspsaga
+    -- use({
+    --   "glepnir/lspsaga.nvim",
+    --   branch = "main",
+    -- })
 
-    -- gitsigns
-    use({
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        require("gitsigns").setup()
-      end,
-    })
+    -- -- gitsigns
+    -- use({
+    --   "lewis6991/gitsigns.nvim",
+    --   config = function()
+    --     require("gitsigns").setup()
+    --   end,
+    -- })
 
-    -- treesitter
-    use({
-      "nvim-treesitter/nvim-treesitter",
-      run = function()
-        local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-        ts_update()
-      end,
-    })
+    -- -- treesitter
+    -- use({
+    --   "nvim-treesitter/nvim-treesitter",
+    --   run = function()
+    --     local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+    --     ts_update()
+    --   end,
+    -- })
 
-    -- statuts line
-    use({
-      "nvim-lualine/lualine.nvim",
-      requires = { "kyazdani42/nvim-web-devicons", opt = true },
-    })
+    -- -- statuts line
+    -- use({
+    --   "nvim-lualine/lualine.nvim",
+    --   requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    -- })
 
-    use {
-      "max397574/better-escape.nvim",
-      config = function()
-        require("better_escape").setup()
-      end,
-    }
+    -- use {
+    --   "max397574/better-escape.nvim",
+    --   config = function()
+    --     require("better_escape").setup()
+    --   end,
+    -- }
 
     if packer_bootstrap then
       require('packer').sync()
