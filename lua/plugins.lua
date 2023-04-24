3760c4ae58071a05a3e169d70167e6de837853ba
MoYuM <hanbaochang12138@gmail.com>
2023-04-24 15:39:35 +0800

    feat: 去掉排序插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -95,6 +95,7 @@ return require("packer").startup({
               end
               return false -- can't save
             end,
+            
             write_all_buffers = false, -- write all buffers when the current one meets `condition`
             debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
             callbacks = { -- functions to be executed at different intervals
@@ -178,8 +179,6 @@ return require("packer").startup({
         require("better_escape").setup()
       end,
     }
-
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
     if packer_bootstrap then
       require('packer').sync()
