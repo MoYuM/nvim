c90effdf10424e5d692140a4fc5cf2067e8b2319
MoYuM <hanbaochang12138@gmail.com>
2023-04-27 18:14:42 +0800

    neovide config

--- init.lua
+++ init.lua
@@ -9,6 +9,24 @@ opt.tabstop = 2
 opt.number = true
 opt.signcolumn = 'yes'
 
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+
+if (vim.g.neovide) then
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.g.neovide_floating_blur_amount_x = 4.0
+  vim.g.neovide_floating_blur_amount_y = 4.0
+  vim.g.neovide_remember_window_size = true
+end
+
+vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h16"
+vim.opt.linespace = 2
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
