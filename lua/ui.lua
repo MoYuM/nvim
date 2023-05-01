aa67a2cb9e568102a9b2efde78aa1231957266a5
MoYuM <hanbaochang12138@gmail.com>
2023-05-01 23:31:04 +0800

    feat: diagnostic sign

--- lua/ui.lua
+++ lua/ui.lua
@@ -10,3 +10,10 @@ opt.number = true
 opt.signcolumn = 'yes'
 
 opt.cursorline = true
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
