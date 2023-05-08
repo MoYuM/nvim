b21d70327036675a9b8fa8a5b663935ba577ca91
MoYuM <hanbaochang12138@gmail.com>
2023-05-08 16:38:26 +0800

    feat: add nvim surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -349,5 +349,16 @@ require('lazy').setup({
     config = function ()
       require("fidget").setup({})
     end
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   }
 })
