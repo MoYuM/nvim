709853477e1e81d2ad995f83ec22fed4b912eeda
MoYuM <wb-hbc831417@alibaba-inc.com>
2023-05-15 10:10:18 +0800

    refactor: 删掉多余代码

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,13 +1,6 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 local lspkind = require("lspkind")
-local copilot_suggestion = require("copilot.suggestion")
-
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 
 local highlight = {
   PmenuSel = { bg = "#282C34", fg = "NONE" },
