099f8b6299d8b46a807f7dc563d0797b8ee7fa23
MoYuM <hanbaochang12138@gmail.com>
2023-05-16 23:07:04 +0800

    feat: neovide config

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,22 +1,20 @@
 local alpha = function()
-  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
 end
 
 if vim.g.neovide then
 
-  -- font
-  vim.o.guifont = "JetBrainsMono Nerd Font:h18"
-  vim.opt.linespace = 2
+  -- suggest font
+  -- JetBrainsMono Nerd Font
+  -- CaskaydiaCove Nerd Font
+  -- suggest size h18
+  vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
 
-  -- neovide_background
+  -- neovide background transparency
   vim.g.neovide_transparency = 0.0
   vim.g.transparency = 0.8
   vim.g.neovide_background_color = "#0f1117" .. alpha()
 
+  -- remember window size
   vim.g.neovide_remember_window_size = true
-
-  -- disable netrw
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
-
 end
