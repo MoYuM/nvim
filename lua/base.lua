5828b04d6349263362968f1c12e641399de84861
MoYuM <hanbaochang12138@gmail.com>
2023-06-09 16:47:00 +0800

    feat: 终于知道为啥了，禁用 editorconfig

--- lua/base.lua
+++ lua/base.lua
@@ -15,3 +15,6 @@ vim.opt.scrolloff = 8
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- disable editorconfig
+vim.g.editorconfig = false
