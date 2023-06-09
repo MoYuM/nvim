fbe24f204ecde76f1d3f1e9b4356da9a522cf8c2
MoYuM <hanbaochang12138@gmail.com>
2023-06-09 16:58:08 +0800

    feat: add neoformat

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,8 +1,8 @@
-local wk = require('which-key')
+local wk = require("which-key")
 
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  vim.keymap.set(mode, lhs, rhs, opts or options)
+	local options = { noremap = true, silent = true }
+	vim.keymap.set(mode, lhs, rhs, opts or options)
 end
 -- leader
 vim.g.mapleader = " "
@@ -13,63 +13,65 @@ map("n", "-", "<C-o>")
 -- restart lsp server
 map("n", "<leader>lr", ":LspRestart<cr>")
 
+map("n", "=", ":Neoformat<cr>")
+
 wk.register({
-  ["3"] = { "#", "find next word under cursor" },
-  ["\\"] = { ":Telescope resume<cr>", "resume telescope" },
+	["3"] = { "#", "find next word under cursor" },
+	["\\"] = { ":Telescope resume<cr>", "resume telescope" },
 })
 
 -- lsp
 wk.register({
-  ["gh"] = { "<cmd>Lspsaga lsp_finder<CR>", "lsp finder" },
-  ["gr"] = { "<cmd>Lspsaga rename<CR>", "rename" },
-  ["gd"] = { "<cmd>Lspsaga peek_definition<CR>", "peek definition"},
-  ["hd"] = { "<cmd>Lspsaga hover_doc<CR>", "hover doc"},
-  ["d]"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>", "jump to next diagnostics"},
-  ["d["] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "jump to prev diagnostics"},
+	["gh"] = { "<cmd>Lspsaga lsp_finder<CR>", "lsp finder" },
+	["gr"] = { "<cmd>Lspsaga rename<CR>", "rename" },
+	["gd"] = { "<cmd>Lspsaga peek_definition<CR>", "peek definition" },
+	["hd"] = { "<cmd>Lspsaga hover_doc<CR>", "hover doc" },
+	["d]"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>", "jump to next diagnostics" },
+	["d["] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "jump to prev diagnostics" },
 })
 
 -- move line
 wk.register({
-  ['<C-j>'] = {':MoveLine(1)<CR>', "move line downo"},
-  ['<C-k>'] = {':MoveLine(-1)<CR>', "move line up"},
-  ['<C-h>'] = {':MoveHChar(-1)<CR>', "move char left"},
-  ['<C-l>'] = {':MoveHChar(1)<CR>', "move char right"},
+	["<C-j>"] = { ":MoveLine(1)<CR>", "move line downo" },
+	["<C-k>"] = { ":MoveLine(-1)<CR>", "move line up" },
+	["<C-h>"] = { ":MoveHChar(-1)<CR>", "move char left" },
+	["<C-l>"] = { ":MoveHChar(1)<CR>", "move char right" },
 })
 
 wk.register({
-  ['a'] = { "ggVG", "Select all" },
-  ["ca"] = { "<cmd>Lspsaga code_action<CR>", "code action" },
-  ["k"] = { "<cmd>Lspsaga show_line_diagnostics<CR>", "show line diagnostics" },
-  ["K"] = {"<cmd>Lspsaga show_buf_diagnostics<CR>", "show buffer diagnostics"},
-  ["O"] = {"<cmd>Lspsaga outline<CR>", 'show outline'},
-  ["gg"] = {"<cmd>LazyGit<CR>", "lazygit"},
-  ["ff"] = { "<cmd>Telescope find_files<cr>", "Find Files" },
-  ["lg"] = {"<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>", "Live Grep" },
-  ["o"] = {"<cmd>lua require('telescope').extensions.recent_files.pick()<cr>", "Recent files" },
-  ["sh"] = {"<cmd>Telescope search_history<cr>", "Buffers" },
-  ["ch"] ={ "<cmd>Telescope command_history<cr>", "Buffers" },
-  ["s"] = { "<cmd>HopChar1<cr>", "Hop" }
+	["a"] = { "ggVG", "Select all" },
+	["ca"] = { "<cmd>Lspsaga code_action<CR>", "code action" },
+	["k"] = { "<cmd>Lspsaga show_line_diagnostics<CR>", "show line diagnostics" },
+	["K"] = { "<cmd>Lspsaga show_buf_diagnostics<CR>", "show buffer diagnostics" },
+	["O"] = { "<cmd>Lspsaga outline<CR>", "show outline" },
+	["gg"] = { "<cmd>LazyGit<CR>", "lazygit" },
+	["ff"] = { "<cmd>Telescope find_files<cr>", "Find Files" },
+	["lg"] = { "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>", "Live Grep" },
+	["o"] = { "<cmd>lua require('telescope').extensions.recent_files.pick()<cr>", "Recent files" },
+	["sh"] = { "<cmd>Telescope search_history<cr>", "Buffers" },
+	["ch"] = { "<cmd>Telescope command_history<cr>", "Buffers" },
+	["s"] = { "<cmd>HopChar1<cr>", "Hop" },
 }, {
-    prefix = '<leader>',
-  })
+	prefix = "<leader>",
+})
 
 wk.register({
-  ['<C-j>'] = {':MoveBlock(1)<CR>', 'move block down'},
-  ['<C-k>'] = {':MoveBlock(-1)<CR>', 'move block up'},
-  ['<C-h>'] = {':MoveHBlock(-1)<CR>', 'move block left'},
-  ['<C-l>'] = {':MoveHBlock(1)<CR>', 'move block right'},
+	["<C-j>"] = { ":MoveBlock(1)<CR>", "move block down" },
+	["<C-k>"] = { ":MoveBlock(-1)<CR>", "move block up" },
+	["<C-h>"] = { ":MoveHBlock(-1)<CR>", "move block left" },
+	["<C-l>"] = { ":MoveHBlock(1)<CR>", "move block right" },
 }, {
-    mode = 'v',
-  })
+	mode = "v",
+})
 
 -- toggle terminal
 wk.register({
-  ['<D-j>'] = {':ToggleTerm direction=float<CR>', 'toggle terminal float'},
+	["<D-j>"] = { ":ToggleTerm direction=float<CR>", "toggle terminal float" },
 }, {
-    mode = 'n'
-  })
+	mode = "n",
+})
 wk.register({
-  ['<esc>'] = {'<C-\\><C-n>', 'quit term mode'},
+	["<esc>"] = { "<C-\\><C-n>", "quit term mode" },
 }, {
-    mode = 't'
-  })
+	mode = "t",
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,381 +1,385 @@
-require('lazy').setup({
-  "nvim-lua/plenary.nvim",
-  "nvim-tree/nvim-web-devicons",
-  "windwp/nvim-ts-autotag",
-  'fedepujol/move.nvim',
-  "RRethy/vim-illuminate",
-  "kdheepak/lazygit.nvim",
-  "smartpde/telescope-recent-files",
-  "onsails/lspkind.nvim",
-  "tpope/vim-fugitive",
-  'nvim-treesitter/nvim-treesitter-context',
-  "Pocco81/auto-save.nvim",
+require("lazy").setup({
+	"nvim-lua/plenary.nvim",
+	"nvim-tree/nvim-web-devicons",
+	"windwp/nvim-ts-autotag",
+	"fedepujol/move.nvim",
+	"RRethy/vim-illuminate",
+	"kdheepak/lazygit.nvim",
+	"smartpde/telescope-recent-files",
+	"onsails/lspkind.nvim",
+	"tpope/vim-fugitive",
+	"nvim-treesitter/nvim-treesitter-context",
+	"Pocco81/auto-save.nvim",
 
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-live-grep-args.nvim",
-    },
-    config = function()
-      require("telescope").setup {
-        defaults = {
-        },
-        pickers = {
-          find_files = {
-            theme = "dropdown",
-          },
-        },
-        extensions = {
-          recent_files = {
-            only_cwd = true,
-            theme = "dropdown",
-          },
-          live_grep_args = {
-            auto_quoting = false,
-            theme = "dropdown"
-          }
-        }
-      }
-      require("telescope").load_extension("recent_files")
-      require("telescope").load_extension("live_grep_args")
-    end
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-live-grep-args.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				defaults = {},
+				pickers = {
+					find_files = {
+						theme = "dropdown",
+					},
+				},
+				extensions = {
+					recent_files = {
+						only_cwd = true,
+						theme = "dropdown",
+					},
+					live_grep_args = {
+						auto_quoting = false,
+						theme = "dropdown",
+					},
+				},
+			})
+			require("telescope").load_extension("recent_files")
+			require("telescope").load_extension("live_grep_args")
+		end,
+	},
 
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    lazy = false,
-    config = function ()
-      require('copilot').setup({
-        suggestion = {
-          accept = false,
-          enable = true,
-          auto_trigger = true,
-          keymap = {
-            accept = "<C-;>"
-          }
-        },
-        panel = { enabled = false },
-      })
-    end
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		lazy = false,
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					accept = false,
+					enable = true,
+					auto_trigger = true,
+					keymap = {
+						accept = "<C-;>",
+					},
+				},
+				panel = { enabled = false },
+			})
+		end,
+	},
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup({
-        transparent = true,
-      })
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+			})
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    keys = {
-      { "1", "<cmd>:NvimTreeFindFileToggle<cr>", desc = "NvimTree" },
-    },
-    config = function()
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-      vim.opt.termguicolors = true
-      require("nvim-tree").setup({
-        sort_by = "name",
-        view = {
-          adaptive_size = true,
-          float = {
-            enable = true,
-          },
-        },
-        renderer = {
-          group_empty = true,
-          highlight_git = true,
-        },
-        actions = {
-          open_file = {
-            quit_on_open = true,
-          },
-        },
-        filters = {
-          dotfiles = true,
-        },
-      })
-    end
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		keys = {
+			{ "1", "<cmd>:NvimTreeFindFileToggle<cr>", desc = "NvimTree" },
+		},
+		config = function()
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
+			vim.opt.termguicolors = true
+			require("nvim-tree").setup({
+				sort_by = "name",
+				view = {
+					adaptive_size = true,
+					float = {
+						enable = true,
+					},
+				},
+				renderer = {
+					group_empty = true,
+					highlight_git = true,
+				},
+				actions = {
+					open_file = {
+						quit_on_open = true,
+					},
+				},
+				filters = {
+					dotfiles = true,
+				},
+			})
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/nvim-cmp"
-    },
-    config = function()
-      require("nvim-autopairs").setup({
-        check_ts = true,
-      })
-      -- setup cmp for autopairs
-      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end
-  },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true,
+			})
+			-- setup cmp for autopairs
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      require("mason").setup()
-      require("mason-lspconfig").setup {
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "cssls"
-        },
-        handlers = {
-          function (server_name)
-            require("lspconfig")[server_name].setup {
-              capabilities = capabilities
-            }
-          end,
-          ["lua_ls"] = function ()
-            require("lspconfig").lua_ls.setup {
-              settings = {
-                Lua = {
-                  diagnostics = {
-                    globals = { "vim" }
-                  }
-                }
-              }
-            }
-          end,
-        }
-      }
-    end
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"cssls",
+				},
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({
+							capabilities = capabilities,
+						})
+					end,
+					["lua_ls"] = function()
+						require("lspconfig").lua_ls.setup({
+							settings = {
+								Lua = {
+									diagnostics = {
+										globals = { "vim" },
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-          "lua",
-          "typescript",
-          "tsx",
-          "javascript",
-          "markdown",
-          "markdown_inline",
-        },
-        highlight = {
-          enable = true,
-          use_languagetree = true,
-        },
-        indent = { enable = true },
-        autotag = {
-          enable = true,
-        }
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"typescript",
+					"tsx",
+					"javascript",
+					"markdown",
+					"markdown_inline",
+				},
+				highlight = {
+					enable = true,
+					use_languagetree = true,
+				},
+				indent = { enable = true },
+				autotag = {
+					enable = true,
+				},
+			})
+		end,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-    },
-    config = function()
-      require('configs.cmp')
-    end,
-  },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			require("configs.cmp")
+		end,
+	},
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      vim.opt.list = true
-      require("indent_blankline").setup {
-        space_char_blankline = " ",
-        show_current_context = true,
-      }
-    end
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			vim.opt.list = true
+			require("indent_blankline").setup({
+				space_char_blankline = " ",
+				show_current_context = true,
+			})
+		end,
+	},
 
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup({
-        mapping = { "jj" },
-      })
-    end,
-  },
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup({
+				mapping = { "jj" },
+			})
+		end,
+	},
 
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "f-person/git-blame.nvim",
-    },
-    config = function()
-      local function moyum()
-        return [[moyum]]
-      end
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"f-person/git-blame.nvim",
+		},
+		config = function()
+			local function moyum()
+				return [[moyum]]
+			end
 
-      local git_blame = require('gitblame')
-      vim.g.gitblame_date_format = '%r'
-      vim.g.gitblame_message_template = '󰙊 <summary>'
-      vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+			local git_blame = require("gitblame")
+			vim.g.gitblame_date_format = "%r"
+			vim.g.gitblame_message_template = "󰙊 <summary>"
+			vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
 
-      require("lualine").setup({
-        options = {
-          theme = "tokyonight",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-        },
-        sections = {
-          lualine_a = {
-            {
-              'mode',
-              fmt = function(str)
-                return ' ' .. str
-              end
-            }
-          },
-          lualine_b = { 'branch', 'diagnostics'},
-          lualine_x = { "diff" },
-          lualine_y = { "filename" },
-          lualine_z = { moyum },
-          lualine_c = {
-            { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
-          }
-        },
-      })
-    end
-  },
+			require("lualine").setup({
+				options = {
+					theme = "tokyonight",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = {
+						{
+							"mode",
+							fmt = function(str)
+								return " " .. str
+							end,
+						},
+					},
+					lualine_b = { "branch", "diagnostics" },
+					lualine_x = { "diff" },
+					lualine_y = { "filename" },
+					lualine_z = { moyum },
+					lualine_c = {
+						{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+					},
+				},
+			})
+		end,
+	},
 
-  {
-    "rmagatti/auto-session",
-    config = function()
-      require("auto-session").setup({
-        log_level = "error",
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      })
-    end,
-  },
+	{
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			})
+		end,
+	},
 
-  {
-    "numToStr/Comment.nvim",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = function()
-      require("Comment").setup({
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-      })
-    end,
-  },
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
 
-  {
-    "glepnir/lspsaga.nvim",
-    event = "LspAttach",
-    config = function()
-      require("lspsaga").setup({
-        beacon = {
-          enable = true,
-          frequency = 7,
-        },
-        ui = {
-          code_action = "",
-        },
-        symbol_in_winbar = {
-          enable = false,
-        }
-      })
-    end,
-    dependencies = {
-      { "nvim-tree/nvim-web-devicons" },
-      { "nvim-treesitter/nvim-treesitter" }
-    }
-  },
+	{
+		"glepnir/lspsaga.nvim",
+		event = "LspAttach",
+		config = function()
+			require("lspsaga").setup({
+				beacon = {
+					enable = true,
+					frequency = 7,
+				},
+				ui = {
+					code_action = "",
+				},
+				symbol_in_winbar = {
+					enable = false,
+				},
+			})
+		end,
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	},
 
-  {
-   "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 
-  {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup({})
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({})
+		end,
+	},
 
-  {
-    "ray-x/lsp_signature.nvim",
-    config = function ()
-      require("lsp_signature").setup({})
-    end
-  },
+	{
+		"ray-x/lsp_signature.nvim",
+		config = function()
+			require("lsp_signature").setup({})
+		end,
+	},
 
-  {
-    "j-hui/fidget.nvim",
-    config = function ()
-      require("fidget").setup({})
-    end
-  },
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup({})
+		end,
+	},
 
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  },
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 
-  {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {
-      autochdir = true,
-    }
-  },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			autochdir = true,
+		},
+	},
 
-  {
-    'Wansmer/treesj',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-      require('treesj').setup({--[[ your config ]]})
-    end,
-  },
+	{
+		"Wansmer/treesj",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesj").setup({--[[ your config ]]
+			})
+		end,
+	},
 
-  {
-    "ggandor/leap.nvim",
-    config = function ()
-      require('leap').add_default_mappings()
-    end
-  },
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
 
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
-    config = function ()
-      require('ufo').setup()
-    end
-  }
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+		config = function()
+			require("ufo").setup()
+		end,
+	},
+
+	{
+		"sbdchd/neoformat",
+	},
 })
