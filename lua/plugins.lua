b4f05b1678e10f7f268b44d54ec87216b1494ace
MoYuM <hanbaochang12138@gmail.com>
2023-07-26 10:27:42 +0800

    feat: add todo plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,7 +23,15 @@ require("lazy").setup({
 			mode = "lsp_references",
 		},
 	},
-
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+    },
 	{
 		"gbprod/yanky.nvim",
 		opts = {},
