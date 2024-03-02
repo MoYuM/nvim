9424ff7442f2b5ec934b780c3572263c71527f01
MoYuM <hanbaochang12138@gmail.com>
2024-03-02 16:33:14 +0800

    feat: delete duck

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -156,19 +156,4 @@ return {
 			})
 		end,
 	},
-
-	{
-		"tamton-aquib/duck.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader>dd", function()
-				require("duck").hatch()
-			end, {})
-			vim.keymap.set("n", "<leader>dk", function()
-				require("duck").cook()
-			end, {})
-			vim.keymap.set("n", "<leader>da", function()
-				require("duck").cook_all()
-			end, {})
-		end,
-	},
 }
