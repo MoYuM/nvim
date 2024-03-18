f61c8e6c5528a649004869d542f0ed174a36f962
MoYuM <hanbaochang12138@gmail.com>
2024-03-18 11:13:17 +0800

    feat: 增加 Neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,4 +10,14 @@ return {
 			require("scrollbar.handlers.gitsigns").setup()
 		end,
 	},
+
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+	},
 }
