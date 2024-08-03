49ec4c45e7c139eb252764a4c34b8ef285ab77c8
MoYuM <hanbaochang12138@gmail.com>
2024-08-03 23:18:44 +0800

    update

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,55 +1,48 @@
 local wk = require("which-key")
 
-wk.register({
-	-- normal
-	["3"] = { "#", "find next word under cursor" },
-	["\\"] = { ":Telescope resume<cr>", "resume telescope" },
-	["<esc>"] = { ":noh<cr>", "no highlight" },
-	["99"] = { "<cmd>lua require('mo').moc()<cr>", "test" },
-	["="] = { ":lua require('conform').format()<cr>", "format" },
-	["<leader>a"] = { "ggVG", "Select all" },
-	["<leader><leader>"] = { "<cmd>Telescope commands<cr>", "Find Commands" },
-  ["<leader>mc"] = { "<cmd>MCunderCursor<cr>", "MCunderCursor" },
-
-	-- terminal
-	["<leader>t"] = { ":ToggleTerm<CR>", "toggle terminal float" },
-
-	-- lsp
-	["gh"] = { "<cmd>Lspsaga lsp_finder<CR>", "lsp finder" },
-	["gd"] = { "<cmd>Lspsaga peek_definition<CR>", "peek definition" },
-	["hd"] = { "<cmd>Lspsaga hover_doc<CR>", "hover doc" },
-	["d]"] = { ":Lspsaga diagnostic_jump_next<CR>", "jump to next diagnostics" },
-	["d["] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "jump to prev diagnostics" },
-	["<leader>ca"] = { "<cmd>Lspsaga code_action<CR>", "code action" },
-	["<leader>k"] = { "<cmd>Lspsaga show_line_diagnostics<CR>", "show line diagnostics" },
-	["<leader>K"] = { "<cmd>Lspsaga show_buf_diagnostics<CR>", "show buffer diagnostics" },
-	["<leader>O"] = { "<cmd>Lspsaga outline<CR>", "show outline" },
-	["<leader>rn"] = { ":IncRename ", "rename" },
-	["<leader>rl"] = { ":LspRestart<cr>", "Restart lsp server" },
-
-	-- move line
-	["<C-j>"] = { ":MoveLine(1)<CR>", "move line down" },
-	["<C-k>"] = { ":MoveLine(-1)<CR>", "move line up" },
-	["<C-h>"] = { ":MoveHChar(-1)<CR>", "move char left" },
-	["<C-l>"] = { ":MoveHChar(1)<CR>", "move char right" },
-
-	["<leader>g"] = { "<cmd>LazyGit<CR>", "git" },
-	["<leader>f"] = { "<cmd>Telescope find_files<cr>", "Find Files" },
-	["<leader>lg"] = { "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>", "Live Grep" },
-	["<leader>o"] = { "<cmd>lua require('telescope').extensions.recent_files.pick()<cr>", "Recent files" },
+wk.add({
+	{ "3", "#", desc = "find next word under cursor" },
+	{ "99", "<cmd>lua require('mo').moc()<cr>", desc = "test" },
+	{ "<C-h>", ":MoveHChar(-1)<CR>", desc = "move char left" },
+	{ "<C-j>", ":MoveLine(1)<CR>", desc = "move line down" },
+	{ "<C-k>", ":MoveLine(-1)<CR>", desc = "move line up" },
+	{ "<C-l>", ":MoveHChar(1)<CR>", desc = "move char right" },
+	{ "<esc>", ":noh<cr>", desc = "no highlight" },
+	{ "<leader><leader>", "<cmd>Telescope commands<cr>", desc = "Find Commands" },
+	{ "<leader>K", "<cmd>Lspsaga show_buf_diagnostics<CR>", desc = "show buffer diagnostics" },
+	{ "<leader>O", "<cmd>Lspsaga outline<CR>", desc = "show outline" },
+	{ "<leader>a", "ggVG", desc = "Select all" },
+	{ "<leader>ca", "<cmd>Lspsaga code_action<CR>", desc = "code action" },
+	{ "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+	{ "<leader>g", "<cmd>LazyGit<CR>", desc = "git" },
+	{ "<leader>k", "<cmd>Lspsaga show_line_diagnostics<CR>", desc = "show line diagnostics" },
+	{
+		"<leader>lg",
+		"<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>",
+		desc = "Live Grep",
+	},
+	{ "<leader>mc", "<cmd>MCunderCursor<cr>", desc = "MCunderCursor" },
+	{ "<leader>o", "<cmd>lua require('telescope').extensions.recent_files.pick()<cr>", desc = "Recent files" },
+	{ "<leader>rl", ":LspRestart<cr>", desc = "Restart lsp server" },
+	{ "<leader>rn", ":IncRename ", desc = "rename" },
+	{ "<leader>t", ":ToggleTerm<CR>", desc = "toggle terminal float" },
+	{ "=", ":lua require('conform').format()<cr>", desc = "format" },
+	{ "\\", ":Telescope resume<cr>", desc = "resume telescope" },
+	{ "d[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "jump to prev diagnostics" },
+	{ "d]", ":Lspsaga diagnostic_jump_next<CR>", desc = "jump to next diagnostics" },
+	{ "gd", "<cmd>Lspsaga peek_definition<CR>", desc = "peek definition" },
+	{ "gh", "<cmd>Lspsaga lsp_finder<CR>", desc = "lsp finder" },
+	{ "hd", "<cmd>Lspsaga hover_doc<CR>", desc = "hover doc" },
 })
 
-wk.register({
-	["<C-j>"] = { ":MoveBlock(1)<CR>", "move block down" },
-	["<C-k>"] = { ":MoveBlock(-1)<CR>", "move block up" },
-	["<C-h>"] = { ":MoveHBlock(-1)<CR>", "move block left" },
-	["<C-l>"] = { ":MoveHBlock(1)<CR>", "move block right" },
-}, {
-	mode = "v",
+wk.add({
+	mode = { "v" },
+	{ "<C-h>", ":MoveHBlock(-1)<CR>", desc = "move block left" },
+	{ "<C-j>", ":MoveBlock(1)<CR>", desc = "move block down" },
+	{ "<C-k>", ":MoveBlock(-1)<CR>", desc = "move block up" },
+	{ "<C-l>", ":MoveHBlock(1)<CR>", desc = "move block right" },
 })
 
-wk.register({
-	["<esc><esc>"] = { "<C-\\><C-n>", "quit term mode" },
-}, {
-	mode = "t",
+wk.add({
+	{ "<esc><esc>", "<C-\\><C-n>", desc = "quit term mode", mode = "t" },
 })
