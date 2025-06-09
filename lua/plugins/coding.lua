8952f5f229ac328bfd7d34969704e172bf982057
MoYuM <64450989+MoYuM@users.noreply.github.com>
2025-06-09 02:26:33 +0000

    feat: 新增 Smear cursor

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,306 +1,297 @@
 ---@diagnostic disable: missing-fields
 ---@type LazyPluginSpec[]
 return {
-	-- 自动补全标签
-	{
-		"windwp/nvim-ts-autotag",
-		config = function()
-			require("nvim-ts-autotag").setup({
-				opts = {
-					enable_close = true, -- Auto close tags
-					enable_rename = true, -- Auto rename pairs of tags
-					enable_close_on_slash = false, -- Auto close on trailing </
-				},
-			})
-		end,
-	},
+  -- 自动补全标签
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,           -- Auto close tags
+          enable_rename = true,          -- Auto rename pairs of tags
+          enable_close_on_slash = false, -- Auto close on trailing </
+        },
+      })
+    end,
+  },
 
-	-- 高亮代码中其他和当前选中的一样的单词
-	{
-		"rrethy/vim-illuminate",
-		event = "insertenter",
-	},
+  -- 高亮代码中其他和当前选中的一样的单词
+  {
+    "rrethy/vim-illuminate",
+    event = "insertenter",
+  },
 
-	-- 最近文件
-	{
-		"smartpde/telescope-recent-files",
-		event = "VeryLazy",
-	},
+  -- 最近文件
+  {
+    "smartpde/telescope-recent-files",
+    event = "VeryLazy",
+  },
 
-	-- 自动保存
-	{
-		"pocco81/auto-save.nvim",
-		event = "VeryLazy",
-	},
+  -- 自动保存
+  {
+    "pocco81/auto-save.nvim",
+    event = "VeryLazy",
+  },
 
-	-- 快速移动代码
-	{
-		"fedepujol/move.nvim",
-		opts = {},
-		event = "VeryLazy",
-	},
+  -- 快速移动代码
+  {
+    "fedepujol/move.nvim",
+    opts = {},
+    event = "VeryLazy",
+  },
 
-	-- format
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				-- Customize or remove this keymap to your liking
-				"=",
-				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
-				end,
-				mode = "n",
-				desc = "Format buffer",
-			},
-		},
-		-- Everything in opts will be passed to setup()
-		opts = {
-			-- Define your formatters
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				javascript = { "prettierd", "prettier" },
-			},
-			-- Set up format-on-save
-			-- format_on_save = { timeout_ms = 500, lsp_fallback = true },
-			-- Customize formatters
-			formatters = {
-				shfmt = {
-					prepend_args = { "-i", "2" },
-				},
-			},
-		},
-		init = function()
-			-- If you want the formatexpr, here is the place to set it
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
-	},
+  -- format
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "=",
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end,
+        mode = "n",
+        desc = "Format buffer",
+      },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { "prettierd", "prettier" },
+      },
+      -- Set up format-on-save
+      -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
 
-	-- 快捷选中更多的 target
-	{
-		"wellle/targets.vim",
-	},
+  -- 快捷选中更多的 target
+  {
+    "wellle/targets.vim",
+  },
 
-	-- 更明显的 TODO 注释
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-		event = "VeryLazy",
-	},
+  -- 更明显的 TODO 注释
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "VeryLazy",
+  },
 
-	-- telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-telescope/telescope-live-grep-args.nvim",
-		},
-		event = "VeryLazy",
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					layout_config = {
-						width = 0.9,
-						height = 0.9,
-					},
-				},
-				pickers = {},
-				extensions = {
-					recent_files = {
-						only_cwd = true,
-					},
-					live_grep_args = {
-						auto_quoting = false,
-					},
-				},
-			})
-			require("telescope").load_extension("recent_files")
-			require("telescope").load_extension("live_grep_args")
-		end,
-	},
+  -- telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_config = {
+            width = 0.9,
+            height = 0.9,
+          },
+        },
+        pickers = {},
+        extensions = {
+          recent_files = {
+            only_cwd = true,
+          },
+          live_grep_args = {
+            auto_quoting = false,
+          },
+        },
+      })
+      require("telescope").load_extension("recent_files")
+      require("telescope").load_extension("live_grep_args")
+    end,
+  },
 
-	-- github copilot
-	{ "AndreM222/copilot-lualine" },
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		lazy = false,
+  -- github copilot
+  { "AndreM222/copilot-lualine" },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    lazy = false,
     enabled = false,
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					accept = false,
-					enable = true,
-					auto_trigger = true,
-					keymap = {
-						accept = "<C-l>",
-					},
-				},
-				panel = { enabled = false },
-			})
-		end,
-	},
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          accept = false,
+          enable = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-l>",
+          },
+        },
+        panel = { enabled = false },
+      })
+    end,
+  },
 
-	-- 自动括号补全,
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-	},
+  -- 自动括号补全,
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
 
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		opts = {},
-	},
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {},
+  },
 
-	-- 自动补全
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"l3mon4d3/luasnip",
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-		},
-		config = function()
-			require("configs.cmp")
-		end,
-		event = { "insertenter", "CmdlineEnter" },
-	},
+  -- 自动补全
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "l3mon4d3/luasnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+    config = function()
+      require("configs.cmp")
+    end,
+    event = { "insertenter", "CmdlineEnter" },
+  },
 
-	-- 更好的 jj 退出
-	{
-		"max397574/better-escape.nvim",
-		event = "insertenter",
-		config = function()
-			require("better_escape").setup()
-		end,
-	},
+  -- 更好的 jj 退出
+  {
+    "max397574/better-escape.nvim",
+    event = "insertenter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
 
-	-- 注释
-	{
-		"numtostr/Comment.nvim",
-		dependencies = {
-			"joosepalviste/nvim-ts-context-commentstring",
-		},
-		config = function()
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
-		end,
-		keys = {
-			"gcc",
-			{
-				"gc",
-				mode = "v",
-			},
-		},
-	},
+  -- 注释
+  {
+    "numtostr/Comment.nvim",
+    dependencies = {
+      "joosepalviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+    keys = {
+      "gcc",
+      {
+        "gc",
+        mode = "v",
+      },
+    },
+  },
 
-	-- 快捷键
-	{
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup({})
-		end,
-	},
+  -- 快捷键
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup({})
+    end,
+  },
 
-	-- 转换对象数组的写法
-	{
-		"wansmer/treesj",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		opts = {},
-		keys = { "<leader>m" },
-	},
+  -- 转换对象数组的写法
+  {
+    "wansmer/treesj",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {},
+    keys = { "<leader>m" },
+  },
 
-	-- 折叠
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = "kevinhwang91/promise-async",
-		config = function()
-			local handler = function(virtText, lnum, endLnum, width, truncate)
-				local newVirtText = {}
-				local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-				local sufWidth = vim.fn.strdisplaywidth(suffix)
-				local targetWidth = width - sufWidth
-				local curWidth = 0
-				for _, chunk in ipairs(virtText) do
-					local chunkText = chunk[1]
-					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-					if targetWidth > curWidth + chunkWidth then
-						table.insert(newVirtText, chunk)
-					else
-						chunkText = truncate(chunkText, targetWidth - curWidth)
-						local hlGroup = chunk[2]
-						table.insert(newVirtText, { chunkText, hlGroup })
-						chunkWidth = vim.fn.strdisplaywidth(chunkText)
-						-- str width returned from truncate() may less than 2nd argument, need padding
-						if curWidth + chunkWidth < targetWidth then
-							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-						end
-						break
-					end
-					curWidth = curWidth + chunkWidth
-				end
-				table.insert(newVirtText, { suffix, "MoreMsg" })
-				return newVirtText
-			end
+  -- 折叠
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    config = function()
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end
 
-			-- global handler
-			require("ufo").setup({
-				enable_get_fold_virt_text = true,
-				fold_virt_text_handler = handler,
-			})
-		end,
-	},
+      -- global handler
+      require("ufo").setup({
+        enable_get_fold_virt_text = true,
+        fold_virt_text_handler = handler,
+      })
+    end,
+  },
 
-	-- 快速跳转
-	{
-		"ggandor/leap.nvim",
-		config = function()
-			require("leap").add_default_mappings()
-		end,
-		keys = { "s", "S" },
-	},
+  -- 快速跳转
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+    keys = { "s", "S" },
+  },
 
-	{
-		"smoka7/multicursors.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"smoka7/hydra.nvim",
-		},
-		opts = {
-			hint_config = {
-				border = "rounded",
-				position = "bottom-right",
-			},
-			generate_hints = {
-				normal = true,
-				insert = true,
-				extend = true,
-				config = {
-					column_count = 1,
-				},
-			},
-		},
-		cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-	},
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      'nvimtools/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+      {
+        mode = { 'v', 'n' },
+        '<Leader>m',
+        '<cmd>MCstart<cr>',
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  },
 
-	-- 粘贴
-	{
-		"gbprod/yanky.nvim",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
+  -- 粘贴
+  {
+    "gbprod/yanky.nvim",
+    opts = {},
+  },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,126 +1,131 @@
 return {
-	------------------- UI ----------------------------
-	"onsails/lspkind.nvim", -- cmp 的样式
-	-- tokyonight theme
-	{
-		"folke/tokyonight.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000,
-		config = function()
-			local transparent = true
-			if vim.g.neovide then
-				transparent = true
-			end
-			require("tokyonight").setup({
-				transparent = transparent,
-				on_highlights = function(hl, c)
-					local prompt = "#2d3149"
-					hl.TelescopeNormal = {
-						bg = c.bg_dark,
-						fg = c.fg_dark,
-					}
-					hl.TelescopeBorder = {
-						bg = c.bg_dark,
-						fg = c.bg_dark,
-					}
-					hl.TelescopePromptNormal = {
-						bg = prompt,
-					}
-					hl.TelescopePromptBorder = {
-						bg = prompt,
-						fg = prompt,
-					}
-					hl.TelescopePromptTitle = {
-						bg = prompt,
-						fg = prompt,
-					}
-					hl.TelescopePreviewTitle = {
-						bg = c.bg_dark,
-						fg = c.bg_dark,
-					}
-					hl.TelescopeResultsTitle = {
-						bg = c.bg_dark,
-						fg = c.bg_dark,
-					}
-				end,
-			})
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
+  ------------------- UI ----------------------------
+  "onsails/lspkind.nvim", -- cmp 的样式
+  -- tokyonight theme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000,
+    config = function()
+      local transparent = true
+      if vim.g.neovide then
+        transparent = true
+      end
+      require("tokyonight").setup({
+        transparent = transparent,
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+          hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+          }
+          hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopePromptNormal = {
+            bg = prompt,
+          }
+          hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+        end,
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 
-	-- 状态条
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "BufEnter",
-		dependencies = {
-			"f-person/git-blame.nvim",
-		},
-		config = function()
-			local ui = require("ui")
-			local function moyum()
-				return [[moyum]]
-			end
-			local git_blame = require("gitblame")
-			vim.g.gitblame_date_format = "%r"
-			vim.g.gitblame_message_template = "󰙊 <summary> • <date> • <author>"
-			vim.g.gitblame_display_virtual_text = 0 -- disable virtual text
-			vim.g.gitblame_date_format = "%r"
+  -- 状态条
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "BufEnter",
+    dependencies = {
+      "f-person/git-blame.nvim",
+    },
+    config = function()
+      local ui = require("ui")
+      local function moyum()
+        return [[moyum]]
+      end
+      local git_blame = require("gitblame")
+      vim.g.gitblame_date_format = "%r"
+      vim.g.gitblame_message_template = "󰙊 <summary> • <date> • <author>"
+      vim.g.gitblame_display_virtual_text = 0 -- disable virtual text
+      vim.g.gitblame_date_format = "%r"
 
-			require("lualine").setup({
-				options = {
-					theme = "tokyonight",
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "", right = "" },
-				},
-				sections = {
-					lualine_a = {
-						{
-							"mode",
-							fmt = function()
-								return ui.icons.Vim
-							end,
-						},
-					},
-					lualine_b = {
-						{
-							"branch",
-							color = {
-								fg = ui.colors.orange,
-								gui = "bold",
-							},
-						},
-					},
-					lualine_x = {
+      require("lualine").setup({
+        options = {
+          theme = "tokyonight",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = {
+            {
+              "mode",
+              fmt = function()
+                return ui.icons.Vim
+              end,
+            },
+          },
+          lualine_b = {
+            {
+              "branch",
+              color = {
+                fg = ui.colors.orange,
+                gui = "bold",
+              },
+            },
+          },
+          lualine_x = {
             "copilot",
-						{
-							"diagnostics",
-							symbols = {
-								error = ui.icons.Error,
-								warn = ui.icons.Warn,
-								hint = ui.icons.Hint,
-								info = ui.icons.Info,
-							},
-						},
-					},
-					lualine_y = {
-						{
-							"diff",
-						},
-					},
-					lualine_z = { moyum },
-					lualine_c = {
-						{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
-					},
-				},
-			})
-		end,
-	},
+            {
+              "diagnostics",
+              symbols = {
+                error = ui.icons.Error,
+                warn = ui.icons.Warn,
+                hint = ui.icons.Hint,
+                info = ui.icons.Info,
+              },
+            },
+          },
+          lualine_y = {
+            {
+              "diff",
+            },
+          },
+          lualine_z = { moyum },
+          lualine_c = {
+            { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+          },
+        },
+      })
+    end,
+  },
 
-	{
-		"MeanderingProgrammer/markdown.nvim",
-		main = "render-markdown",
-		opts = {},
-		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-	},
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    opts = {},
+    name = "render-markdown",                                                          -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
+
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+  }
 }
